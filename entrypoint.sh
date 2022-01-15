74f2760d90584f8d767b3e6412ff8ffcaa129825
Josh Wickham <josh@i-josh.com>
2022-01-14 20:08:17 -0800

    update url to listen to everything

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-dotnet run --urls "http://0.0.0.0:5000" --project mvc/mvc.csproj --environment=Docker
+dotnet run --urls "http://*:5000" --project mvc/mvc.csproj --environment=Docker
 #tail -f /dev/null
