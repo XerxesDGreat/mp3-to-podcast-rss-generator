2f3fc3b2db7fb0bc2c2853fb7a56b2a4a896c2d6
Josh Wickham <josh@i-josh.com>
2022-01-14 19:48:12 -0800

    change port for docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-dotnet run --urls "http://0.0.0.0:80" --project mvc/mvc.csproj --environment=Docker
+dotnet run --urls "http://0.0.0.0:5000" --project mvc/mvc.csproj --environment=Docker
 #tail -f /dev/null
